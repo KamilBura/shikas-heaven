4bec5224f8dffcbc63784c1382228cb3995de2a3
Shikaru-sama <kamilshikarubura@gmail.com>
2022-09-23 18:23:40 +0200

    Update 23.09 New About Socialmedia bar

---
+++ js/jquery-about-ann.js
@@ -0,0 +1,84 @@
+function inannfacebook() {
+    var element = document.getElementById("facebook-text");
+    if (element.classList.contains("out")) {
+        element.classList.remove("out")
+    }
+    element.classList.add("in");
+}
+
+function outannfacebook() {
+    var element = document.getElementById("facebook-text");
+    if (element.classList.contains("in")) {
+        element.classList.remove("in")
+    }
+    element.classList.add("out");
+}
+
+function inannsteam() {
+    var element = document.getElementById("steam-text");
+    if (element.classList.contains("out")) {
+        element.classList.remove("out")
+    }
+    element.classList.add("in");
+}
+
+function outannsteam() {
+    var element = document.getElementById("steam-text");
+    if (element.classList.contains("in")) {
+        element.classList.remove("in")
+    }
+    element.classList.add("out");
+}
+
+function inanninstagram() {
+    var element = document.getElementById("instagram-text");
+    if (element.classList.contains("out")) {
+        element.classList.remove("out")
+    }
+    element.classList.add("in");
+}
+
+function outanninstagram() {
+    var element = document.getElementById("instagram-text");
+    if (element.classList.contains("in")) {
+        element.classList.remove("in")
+    }
+    element.classList.add("out");
+}
+
+function inanntwitch() {
+    var element = document.getElementById("twitch-text");
+    if (element.classList.contains("out")) {
+        element.classList.remove("out")
+    }
+    element.classList.add("in");
+}
+
+function outanntwitch() {
+    var element = document.getElementById("twitch-text");
+    if (element.classList.contains("in")) {
+        element.classList.remove("in")
+    }
+    element.classList.add("out");
+}
+
+function inanntiktok() {
+    var element = document.getElementById("tiktok-text");
+    if (element.classList.contains("out")) {
+        element.classList.remove("out")
+    }
+    element.classList.add("in");
+}
+
+function outanntiktok() {
+    var element = document.getElementById("tiktok-text");
+    if (element.classList.contains("in")) {
+        element.classList.remove("in")
+    }
+    element.classList.add("out");
+}
+
+
+
+
+
