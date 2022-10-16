add04a7cb15481166f16318f41df8ba4dd313560
Kamil Bura <88596098+KamilBura@users.noreply.github.com>
2022-10-16 22:12:33 +0200

    16.10.2022 | Some Code Fixes / Cleaning

--- js/look-if-empty.js
+++ js/look-if-empty.js
@@ -1,15 +1,15 @@
 function validateForm() {
   var a = document.forms["report-form"]["ingame-name"].value;
   var b = document.forms["report-form"]["_replyto"].value;
-  var c = document.forms["report-form"]["reason"].value;
-  var d = document.forms["report-form"]["message"].value;
-  if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") { 
-    alert("Please Fill All Required Field");
+  var c = document.forms["report-form"]["message"].value;
+  var d = document.forms["report-form"]["reason"].value;
+  if (a == null || a == "", b == null || b == "", c == null || c == "", d == undefined || d == "") { 
+    alert("Please Fill All Required Field!");
     document.getElementById('submit-btn').classList.add('btn-danger');
     return false;
   } else {
     document.getElementById('submit-btn').classList.add('btn-success');
-    alert("You will be redirected to the Verification Site!");
+    alert("You will be redirected to the Verification Site! After Succesfully Verifiaction the E-Mail will be Sended.");
     return false;
   }
 };
